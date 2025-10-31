66fb6c44bb1fa9de4dd30d8702c58dec49bc0704
User <astakamokro@yandex.ru>
2025-10-31 19:33:52 +0300

    comment all and fix answers

--- src/main/java/com/ecosystem/gateway/filter/ValidationFilter.java
+++ src/main/java/com/ecosystem/gateway/filter/ValidationFilter.java
@@ -16,10 +16,21 @@ import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
+/*
+Фильтр, защищающий зону /api приложения. Проверяет, содержит ли запрос access token, после обращения к auth серверу формирует secutity context
+- если токена нет, помечает запрос как гостевой (headers - role), после чего внутри структуры api формируется только та часть информации, что может быть доступна гостям
+- Если токен есть, и он действителен - формируется security context в headers (username, role, user uuid).
+Помним, что все данные в системе формируются вокруг user uuid
+- Если токен есть, но он просрочен - возвращается 401, после чего фронтенд делает запрос на refresh
+ */
+
 @Component
 public class ValidationFilter extends AbstractGatewayFilterFactory<Object> {
 
+    // реактивный клиент для взаимодействия с auth
     private final WebClient webClient;
+
+    // адрес auth сервера (пока hardcoded)
     private final String authUrl = "http://localhost:8082";
 
 
@@ -37,10 +48,11 @@ public class ValidationFilter extends AbstractGatewayFilterFactory<Object> {
     public GatewayFilter apply(Object config) {
         return (exchange, chain) -> {
 
+            // пытаемся извлечь авторизационный токен типа bearer (с англ - предъявитель)
             String token =
                     exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
 
-            // токена нет - перед нами гость. Вносим инфу об этом в security context
+            // токена нет - перед нами гость. Вносим инфу (mutate) об этом в security context, после чего пробрасываем запрос дальше
             if(token == null || !token.startsWith("Bearer ") || token.length() <= 7) {
 
                 ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
@@ -57,8 +69,11 @@ public class ValidationFilter extends AbstractGatewayFilterFactory<Object> {
                     .uri("/validate")
                     .header(HttpHeaders.AUTHORIZATION, token)
                     .retrieve()
+                    // переводим тело ответа в читаемый вид - Map<String, String>
                     .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+                    // сценарий 1 - токен валиден, получен контекст - добавляем headers
                     .flatMap(body->upgradeRequest(body, exchange, chain))
+                    // сценарий 2 - ошибка, токен не валиден, возвращаем 401
                     .onErrorResume(exception->{
                         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
                         return exchange.getResponse().setComplete();
@@ -67,7 +82,7 @@ public class ValidationFilter extends AbstractGatewayFilterFactory<Object> {
         };
     }
 
-    // добавляем security context при успешной валидации токена
+    // добавляем для запроса к api security context при успешной валидации токена
     private Mono<Void> upgradeRequest(Map<String, Object> body, ServerWebExchange exchange, GatewayFilterChain chain){
         ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
                 .header("role", (String) body.get("role"))
