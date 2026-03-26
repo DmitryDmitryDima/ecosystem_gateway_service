6f479593978d6e4a02e492b9f4378387d5924dbe
admin <astakamokro@yandex.ru>
2026-03-26 19:01:39 +0300

    change running machine

--- src/main/java/com/ecosystem/gateway/GatewayApplication.java
+++ src/main/java/com/ecosystem/gateway/GatewayApplication.java
@@ -2,9 +2,9 @@ package com.ecosystem.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class GatewayApplication {
+	//
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);//
